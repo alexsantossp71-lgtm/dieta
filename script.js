730661b0dabadd87a9fff72b0a40c10a96a69ff5
alexsantossp71-lgtm <alexsantossp71@gmail.com>
2025-12-09 12:25:44 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('action-btn').addEventListener('click', () => {
+    alert('Funcionalidade de teste ativa! O JavaScript está funcionando.');
+});
+
+console.log('Página de teste carregada com sucesso.');
